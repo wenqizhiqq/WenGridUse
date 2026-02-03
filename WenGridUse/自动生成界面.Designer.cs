eccacbe9648d878894a2e1a4f5f4a2beccd48b2e
wenqizhiqq <2423418640@qq.com>
2026-02-03 09:07:15 +0800

    大小

--- WenGridUse/自动生成界面.Designer.cs
+++ WenGridUse/自动生成界面.Designer.cs
@@ -69,7 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 407);
+            this.ClientSize = new System.Drawing.Size(642, 414);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.autoFieldPanel1);
